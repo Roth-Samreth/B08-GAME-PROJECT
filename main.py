7c4309f7f685d8fba5af5a14fbbebd4053e00df0
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2023-10-22 10:40:55 +0700

    second update

--- main.py
+++ main.py
@@ -1,34 +1,62 @@
-# library
 from tkinter import *
-from PIL import Image,ImageTk
-
 # constant
-
-SCREEN_WIDTH = 1536
-SCREEN_HEIGHT = 864
-HEALTH = 100
-health = ""
-TIMELOOP = 10
-GRAVITY_FORCE = 9
-JUMP_FORCE = 30
+SCREENWIDTH = 1150
+SCREENHEIGHT = 600
+GRAVITY_FORCE = 30
 SPEED = 7
-
 TIMED_LOOP = 10
+JUMP_FORCE = 30
 # Variable
 keyPressed = []
+x = 20
+y = 520
+root = Tk(str(SCREENWIDTH)+"x"+str(SCREENHEIGHT))
+canvas = Canvas(root,width=1150,height=600,scrollregion=(0,0,4000,5000))
+back = PhotoImage(file="Image/10.png")
+bg = canvas.create_image(20,20,image=back)
+pp = PhotoImage(file="Image/Run1.png")
+player = canvas.create_image(45,430,image=pp)
+# _________________________________________________________Enemy__________________________________________
 
-# functions
+pf1 = PhotoImage(file="Image/Fighter.png")
+enemy = canvas.create_image(400,350,image=pf1)
+
+# Create platform
+tile1 = PhotoImage(file="Image/Tile_01.png")
+for j in range(3):
+    for i in range(8):
+        tiles = canvas.create_image(x, y, image=tile1, tag = 'PLATFORM')
+        x += 30
+    x=20
+    y+=30
+x1=400
+y1=420
+for n in range(8):
+    y1=420
+    for m in range(2):
+        tiles = canvas.create_image(x1, y1, image=tile1, tag = 'PLATFORM')
+        y1 += 30
+    x1+=30
+x2=x1+150
+y2=320
+for n in range(8):
+    y2=320
+    for m in range(2):
+        tiles = canvas.create_image(x2, y2, image=tile1, tag = 'PLATFORM')
+        y2 += 30
+    x2+=30
+# Funtions
 def check_movement(dx=0, dy=0, checkGround=False):
-    coord = canvas.coords(player)
+    coord = canvas.bbox(player)
     platforms = canvas.find_withtag("PLATFORM")
 
-    if coord[0] + dx < 0 or coord[0] + dx > SCREEN_WIDTH:
+    if coord[0] + dx < 0 or coord[2] + dx > SCREENWIDTH:
         return False
 
     if checkGround:
-        overlap = canvas.find_overlapping(coord[0], coord[1], coord[0], coord[1] + dy)
+        overlap = canvas.find_overlapping(coord[0], coord[1], coord[2], coord[3] + dy)
     else:
-        overlap = canvas.find_overlapping(coord[0]+dx, coord[1], coord[0], coord[1]+dy)
+        overlap = canvas.find_overlapping(coord[0]+dx, coord[1]+dy, coord[2]+dx, coord[3])
 
     for platform in platforms:
         if platform in overlap:
@@ -40,12 +68,6 @@ def jump(force):
         if check_movement(0, -force):
             canvas.move(player, 0, -force)
             root.after(TIMED_LOOP, jump, force-1)
-
-def gravity():
-    if check_movement(0, GRAVITY_FORCE, True):
-        canvas.move(player, 0, GRAVITY_FORCE)
-    root.after(TIMED_LOOP, gravity)
-
 def move():
     if not keyPressed == []:
         x = 0
@@ -63,58 +85,19 @@ def start_move(event):
         keyPressed.append(event.keysym)
         if len(keyPressed) == 1:
             move()
-
 def stop_move(event):
     global keyPressed
     if event.keysym in keyPressed:
         keyPressed.remove(event.keysym)
-# window
-
-root = Tk()
-root.title("Shinobi Run")
-root.geometry(str(SCREEN_WIDTH) +"x"+ str(SCREEN_HEIGHT))
-# create canvas
-canvas = Canvas(root,width=SCREEN_WIDTH,height=SCREEN_HEIGHT)
-# background
-background = Image.open('./Image/10.png')
-bg = ImageTk.PhotoImage(background)
-Background = canvas.create_image(0,0,image=bg,anchor="nw")
-# player
-player_pho = Image.open('Image/Run1.png')
-player_image = ImageTk.PhotoImage(player_pho)
-player=canvas.create_image(100,700,image=player_image)
-canvas.create_text(50,10,text= "HEALTH "+str(HEALTH))
-
-# platform
-
-canvas.create_rectangle(0, 800, SCREEN_WIDTH, SCREEN_HEIGHT, fill="black", tags="PLATFORM")
-canvas.create_rectangle(200, 600, 500, 650, fill="black", tags="PLATFORM")
-canvas.create_rectangle(600, 700, 650, 850, fill="black", tags="PLATFORM")
-canvas.create_rectangle(800, 450, 1100, 500, fill="black", tags="PLATFORM")
-
-canvas.pack(expand=True, fill='both')
+def enemove():
+    canvas.move(player, GRAVITY_FORCE, 0)
+    root.after(TIMED_LOOP, enemove)
+def gravity():
+    if check_movement(0, GRAVITY_FORCE, True):
+        canvas.move(player, 0, GRAVITY_FORCE)
+    root.after(TIMED_LOOP, gravity)
+canvas.pack()
 gravity()
-root.bind("<Key>", start_move)
+root.bind("<Key>",start_move)
 root.bind("<KeyRelease>", stop_move)
-root.mainloop()
-
-
-
-root =  tk.Tk()
-root.title("Shinobi Run")
-root.geometry(str(SCREEN_WIDTH) +"x"+ str(SCREEN_HEIGHT))
-frame = tk.Frame(root,width=SCREEN_WIDTH,height=SCREEN_HEIGHT)
-# create canvas
-canvas = tk.Canvas(frame,width=SCREEN_WIDTH,height=SCREEN_HEIGHT)
-# Background
-img = Image.open("Image/10.png")
-bg = ImageTk.PhotoImage(img)
-Background = canvas.create_image(0,0,image=bg,anchor="nw")
-canvas.create_text(750,250,text="Shinobi Run",font=('Metal Mania',80))
-btn1 = tk.Button(canvas, text = 'New game',font=('Metal Mania',20),command = root.destroy)
-btn1.place(x=700,y=300)
-btn = tk.Button(canvas, text = 'Exit',font=('Metal Mania',20),)
-btn.place(x=725,y=400)
-frame.pack(expand=True, fill='both')
-canvas.pack(expand=True, fill='both')
 root.mainloop()
