bec9069869b9c6a3cc8d9f76513e5e0e342c0749
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2023-10-23 08:44:34 +0700

    Update

--- Home.py
+++ Home.py
@@ -1,26 +1,115 @@
-# library
-import tkinter as tk
+# ____________________Import library
+from tkinter import *
 from PIL import Image,ImageTk
-from tkinter import messagebox
-# constant
 
-SCREEN_WIDTH = 1536
-SCREEN_HEIGHT = 864
-root =  tk.Tk()
-root.title("Shinobi Run")
-root.geometry(str(SCREEN_WIDTH) +"x"+ str(SCREEN_HEIGHT))
-frame = tk.Frame(root,width=SCREEN_WIDTH,height=SCREEN_HEIGHT)
-# create canvas
-canvas = tk.Canvas(frame,width=SCREEN_WIDTH,height=SCREEN_HEIGHT)
-# Background
-img = Image.open("Image/10.png")
-bg = ImageTk.PhotoImage(img)
-Background = canvas.create_image(0,0,image=bg,anchor="nw")
-canvas.create_text(750,250,text="Shinobi Run",font=('Metal Mania',80))
-btn1 = tk.Button(canvas, text = 'New game',font=('Metal Mania',20),command = root.destroy)
-btn1.place(x=700,y=300)
-btn = tk.Button(canvas, text = 'Exit',font=('Metal Mania',20),)
-btn.place(x=725,y=400)
-frame.pack(expand=True, fill='both')
-canvas.pack(expand=True, fill='both')
+# ___________________Constant
+SCREENWIDTH = 1150
+SCREENHEIGHT = 600
+GRAVITY_FORCE = 30
+SPEED = 7
+TIMED_LOOP = 10
+JUMP_FORCE = 30
+
+# Variable
+keyPressed = []
+x = 20
+y = 430
+
+# ____________________Create window
+root = Tk()
+root.geometry(str(SCREENWIDTH)+"x"+str(SCREENHEIGHT))
+canvas = Canvas(root,width= SCREENWIDTH,height=SCREENHEIGHT)
+# Load image
+bg = Image.open("Image/10.png")
+pp = Image.open("Image/Run1.png")
+en = Image.open("Image/Fighter.png")
+tile = Image.open("Image/Tile_01.png")
+
+# Converting Image
+back = ImageTk.PhotoImage(bg)
+user = ImageTk.PhotoImage(pp)
+oppose = ImageTk.PhotoImage(en)
+tile_1 = ImageTk.PhotoImage(tile)
+# Put the Background
+backGround = canvas.create_image(0,0,image=back)
+# Functions
+def create_platform(x,y,row,col):
+    X = x
+    Y = y
+    for j in range(row):
+        for i in range(col):
+            tiles = canvas.create_image(X, Y, image=tile_1, tag='PLATFORM')
+            X += 30
+        X = x
+        Y += 30
+def check_movement(dx=0, dy=0, checkGround=False):
+    global player
+    coord = canvas.bbox(player)
+    platforms = canvas.find_withtag("PLATFORM")
+
+    if coord[0] + dx < 0 or coord[2] + dx > SCREENWIDTH:
+        return False
+
+    if checkGround:
+        overlap = canvas.find_overlapping(coord[0], coord[1], coord[2], coord[3] + dy)
+    else:
+        overlap = canvas.find_overlapping(coord[0]+dx, coord[1]+dy, coord[2]+dx, coord[3])
+
+    for platform in platforms:
+        if platform in overlap:
+            return False
+
+    return True
+def jump(force):
+    if force > 0:
+        if check_movement(0, -force):
+            canvas.move(player, 0, -force)
+        root.after(TIMED_LOOP, jump, force-1)
+def move():
+    if not keyPressed == []:
+        x = 0
+        if "Left" in keyPressed:
+            x -= SPEED
+        if "Right" in keyPressed:
+            x += SPEED
+        if "space" in keyPressed and not check_movement(0, GRAVITY_FORCE, True):
+            jump(JUMP_FORCE)
+        if check_movement(x):
+            canvas.move(player, x, 0)
+        root.after(TIMED_LOOP, move)
+def start_move(event):
+    if event.keysym not in keyPressed:
+        keyPressed.append(event.keysym)
+        if len(keyPressed) == 1:
+            move()
+def stop_move(event):
+    global keyPressed
+    if event.keysym in keyPressed:
+        keyPressed.remove(event.keysym)
+def gravity():
+    if check_movement(0, GRAVITY_FORCE, True):
+        canvas.move(player, 0, GRAVITY_FORCE)
+    root.after(TIMED_LOOP, gravity)
+def level_1():
+    global backGround,player
+    canvas.delete("all")
+    btn.destroy()
+    exitBtn.destroy()
+    backGround = canvas.create_image(0,0,image=back)
+    player = canvas.create_image(45, 340, image=user)
+    create_platform(20, 400, 5, 8)
+    create_platform(340, 300, 4, 8)
+    create_platform(650, 400, 1, 4)
+    create_platform(800, 560, 2, 8)
+    gravity()
+# Game Start Screen
+title = canvas.create_text(580,200,text="Shinobi Run",font=("Metal Mania",50),fill="white")
+# Button
+btn = Button(text="Newgame",font=("Metal Mania",15),command=level_1)
+exitBtn = Button(text="Exit",font=("Metal Mania",15),command=root.destroy)
+btn.place(x=530,y=300)
+exitBtn.place(x=550,y=380)
+canvas.pack(expand = True,fill='both')
+root.bind("<Key>", start_move)
+root.bind("<KeyRelease>", stop_move)
 root.mainloop()
--- main.py
+++ main.py
@@ -10,81 +10,100 @@ JUMP_FORCE = 30
 keyPressed = []
 x = 20
 y = 430
-root = Tk(str(SCREENWIDTH)+"x"+str(SCREENHEIGHT))
-canvas = Canvas(root,width=1150,height=600,scrollregion=(0,0,4000,5000))
-back = PhotoImage(file="Image/10.png")
-bg = canvas.create_image(20,20,image=back)
-pp = PhotoImage(file="Image/Run1.png")
-player = canvas.create_image(45,300,image=pp)
-# _________________________________________________________Enemy__________________________________________
-pf1 = PhotoImage(file="Image/Fighter.png")
 
-# Create platform
-tile1 = PhotoImage(file="Image/Tile_01.png")
-def create_platform(x,y,row,col):
-    X = x
-    Y = y
-    for j in range(row):
-        for i in range(col):
-            tiles = canvas.create_image(X, Y, image=tile1, tag='PLATFORM')
-            X += 30
+
+def gamescreen():
+    root = Toplevel(window)
+    canvas = Canvas(root, width=1150, height=600, scrollregion=(0, 0, 4000, 5000))
+    back = PhotoImage(file="Image/10.png")
+    pp = PhotoImage(file="Image/Run1.png")
+    bg = canvas.create_image(20, 20, image=back)
+    player = canvas.create_image(45, 300, image=pp)
+    # _______________________________________________Enemy__________________________________________
+    pf1 = PhotoImage(file="Image/Fighter.png")
+
+    # Create platform
+    tile1 = PhotoImage(file="Image/Tile_01.png")
+
+    def create_platform(x, y, row, col):
         X = x
-        Y += 30
-create_platform(20,400,5,8)
-create_platform(340,300,4,8)
-create_platform(650,400,1,4)
-create_platform(800,560,2,8)
-# Funtions
-def check_movement(dx=0, dy=0, checkGround=False):
-    coord = canvas.bbox(player)
-    platforms = canvas.find_withtag("PLATFORM")
+        Y = y
+        for j in range(row):
+            for i in range(col):
+                tiles = canvas.create_image(X, Y, image=tile1, tag='PLATFORM')
+                X += 30
+            X = x
+            Y += 30
 
-    if coord[0] + dx < 0 or coord[2] + dx > SCREENWIDTH:
-        return False
+    create_platform(20, 400, 5, 8)
+    create_platform(340, 300, 4, 8)
+    create_platform(650, 400, 1, 4)
+    create_platform(800, 560, 2, 8)
 
-    if checkGround:
-        overlap = canvas.find_overlapping(coord[0], coord[1], coord[2], coord[3] + dy)
-    else:
-        overlap = canvas.find_overlapping(coord[0]+dx, coord[1]+dy, coord[2]+dx, coord[3])
+    # Funtions
+    def check_movement(dx=0, dy=0, checkGround=False):
+        coord = canvas.bbox(player)
+        platforms = canvas.find_withtag("PLATFORM")
 
-    for platform in platforms:
-        if platform in overlap:
+        if coord[0] + dx < 0 or coord[2] + dx > SCREENWIDTH:
             return False
 
-    return True
-def jump(force):
-    if force > 0:
-        if check_movement(0, -force):
-            canvas.move(player, 0, -force)
-            root.after(TIMED_LOOP, jump, force-1)
-def move():
-    if not keyPressed == []:
-        x = 0
-        if "Left" in keyPressed:
-            x -= SPEED
-        if "Right" in keyPressed:
-            x += SPEED
-        if "space" in keyPressed and not check_movement(0, GRAVITY_FORCE, True):
-            jump(JUMP_FORCE)
-        if check_movement(x):
-            canvas.move(player, x, 0)
-        root.after(TIMED_LOOP, move)
-def start_move(event):
-    if event.keysym not in keyPressed:
-        keyPressed.append(event.keysym)
-        if len(keyPressed) == 1:
-            move()
-def stop_move(event):
-    global keyPressed
-    if event.keysym in keyPressed:
-        keyPressed.remove(event.keysym)
+        if checkGround:
+            overlap = canvas.find_overlapping(coord[0], coord[1], coord[2], coord[3] + dy)
+        else:
+            overlap = canvas.find_overlapping(coord[0] + dx, coord[1] + dy, coord[2] + dx, coord[3])
 
-def gravity():
-    if check_movement(0, GRAVITY_FORCE, True):
-        canvas.move(player, 0, GRAVITY_FORCE)
-    root.after(TIMED_LOOP, gravity)
-canvas.pack()
-gravity()
-root.bind("<Key>",start_move)
-root.bind("<KeyRelease>", stop_move)
-root.mainloop()
+        for platform in platforms:
+            if platform in overlap:
+                return False
+
+        return True
+
+    def jump(force):
+        if force > 0:
+            if check_movement(0, -force):
+                canvas.move(player, 0, -force)
+                root.after(TIMED_LOOP, jump, force - 1)
+
+    def move():
+        if not keyPressed == []:
+            x = 0
+            if "Left" in keyPressed:
+                x -= SPEED
+            if "Right" in keyPressed:
+                x += SPEED
+            if "space" in keyPressed and not check_movement(0, GRAVITY_FORCE, True):
+                jump(JUMP_FORCE)
+            if check_movement(x):
+                canvas.move(player, x, 0)
+            root.after(TIMED_LOOP, move)
+
+    def start_move(event):
+        if event.keysym not in keyPressed:
+            keyPressed.append(event.keysym)
+            if len(keyPressed) == 1:
+                move()
+
+    def stop_move(event):
+        global keyPressed
+        if event.keysym in keyPressed:
+            keyPressed.remove(event.keysym)
+
+    def gravity():
+        if check_movement(0, GRAVITY_FORCE, True):
+            canvas.move(player, 0, GRAVITY_FORCE)
+        root.after(TIMED_LOOP, gravity)
+
+    canvas.pack()
+    gravity()
+    canvas.bind_all("<Key>", start_move)
+    canvas.bind_all("<KeyRelease>", stop_move)
+    root.mainloop()
+def startscreen():
+    global window
+    window = Tk()
+    window.geometry("400x400")
+    btn = Button(window, text="Play Game", command=gamescreen,font=("Metal Mania",15))
+    btn.place(x=150,y=150)
+    window.mainloop()
+startscreen()
